c085835b001a6c82304d700e79ac036264f2e51c
Misael M. Morales <misaelmiguelmorales@gmail.com>
2024-03-05 20:28:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+############################################################################
+#        AUTOMATED ANISOTROPIC RESISTIVITY INVERSION FOR EFFICIENT         #
+#           FORMATION EVALUATION AND UNCERTAINTY QUANTIFICATION            #
+############################################################################
+# Author: Misael M. Morales (github.com/misaelmmorales)                    #
+# Co-Authors: Oriyomi Raheem, Ali Eghbali - UT Austin                      #
+# Co-Authors: Dr. Michael Pyrcz, Dr. Carlos Torres-Verdin - UT Austin      #
+# Date: 2024-03-01                                                         #
+############################################################################
+# Copyright (c) 2024, Misael M. Morales                                    #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#     http://www.apache.org/licenses/LICENSE-2.0                           #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+
 import time
 import numpy as np
 import pandas as pd
