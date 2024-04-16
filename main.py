39ee8bf603df5f015921f5b552ac5816b98c0c7d
Misael M. Morales <misaelmiguelmorales@gmail.com>
2024-04-15 21:30:11 -0500

    update

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 from matplotlib.animation import FuncAnimation
 
 import lasio
