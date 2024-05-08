b3303eb8489b7a0b711fc3cbb76bdb9b09bd0321
Misael M. Morales <misaelmiguelmorales@gmail.com>
2024-05-08 01:06:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import os
 import time
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from matplotlib.animation import FuncAnimation
